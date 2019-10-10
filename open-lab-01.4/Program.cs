fa98e70a564a7e2c536ee321970d60934e02f2e2
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-10 09:10:49 +0200

    open lab 01.4

--- open-lab-01.4/Program.cs
+++ open-lab-01.4/Program.cs
@@ -6,10 +6,11 @@ namespace open_lab_01._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the base of the triangle first and then enter the height of the triangle");
+            Console.WriteLine("Enter the base of the triangle.");
             string siteA = Console.ReadLine();
-            string height = Console.ReadLine();
             double a = Convert.ToDouble(siteA);
+            Console.WriteLine("Enter the height of the triangle");
+            string height = Console.ReadLine();
             double b = Convert.ToDouble(height);
             double c = a * b / 2;
             Console.WriteLine($"The area is {c}!");
